49618b756585ba5fefe64c1fda2974592d5d78f0
chohamin <88224574+chohamin@users.noreply.github.com>
2024-06-25 21:55:15 +0900

    6.25 최종 코드

--- src/App.js
+++ src/App.js
@@ -1,56 +1,81 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie.js';
+// import Home from './routes/home';
 import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-class App extends React.Component {
-  
-  state = {
-    isloading: true,
-    movies: []
-  };
-
-  componentDidMount() {
-    // 영화 데이터 로딩!
-    this.getMovies();
-  }
-  getMovies = async () => {
-    const {
-      data : {
-        data : { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isloading: false });
-  }
-
-  render() {
-    const { isloading, movies } = this.state;
-    return (
-      <section class="container">
-        { isloading ? (
-          <div class="loader">
-            <span class="loader_text">'loading...'</span>
-          </div>
-        ) :(
-          <div class='movies'>
-          {movies.map(movie => (
-            <Movie
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-            />
-        ))}
-        </div>
-      )}
-      </section>
-    );
-   }
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Routes>
+      <Route Path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component={Detail}/>
+      </Routes>
+    </HashRouter>
+  );
+}
 
 
 export default App;
+
+// import axios from 'axios';
+// import Movie from './components/Movie.js';
+// import './App.css';
+
+// class App extends React.Component {
+  
+//   state = {
+//     isloading: true,
+//     movies: []
+//   };
+
+//   componentDidMount() {
+//     // 영화 데이터 로딩!
+//     this.getMovies();
+//   }
+//   getMovies = async () => {
+//     const {
+//       data : {
+//         data : { movies }
+//       }
+//     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+//     this.setState({ movies, isloading: false });
+//   }
+
+//   render() {
+//     const { isloading, movies } = this.state;
+//     return (
+//       <section class="container">
+//         { isloading ? (
+//           <div className="loader">
+//             <span className="loader_text">'loading...'</span>
+//           </div>
+//         ) :(
+//           <div className='movies'>
+//           {movies.map(movie => (
+//             <Movie
+//               id={movie.id}
+//               year={movie.year}
+//               title={movie.title}
+//               summary={movie.summary}
+//               poster={movie.medium_cover_image}
+//               genres={movie.genres}
+//             />
+//         ))}
+//         </div>
+//       )}
+//       </section>
+//     );
+//    }
+//   }
+
+
+// export default App;
 
 
 
---
+++ src/routes/Detail.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
+
+function Detail() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (location.state == null) {
+            return navigate('/');
+        }
+    }, [])
+
+    if (location.state) {
+        return <span>{location.state.title}</span>;
+    } else {
+        return null;
+    }
+}
+// function Detail(props) {
+//     console.log(props);
+    // return <span>{location.state.title}</span>;
+
+
+export default Detail;
